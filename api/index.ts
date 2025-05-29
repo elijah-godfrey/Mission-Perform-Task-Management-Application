e5c734f9622ca44ca5cbcf59418f09dc0c421872
Elijah Godfrey <egodf1@gmail.com>
2025-05-29 09:07:13 -0400

    Add api directory for Vercel serverless functions

---
+++ api/index.ts
@@ -0,0 +1,6 @@
+// This file serves as the entry point for Vercel serverless functions
+// It imports the Express app from the server directory
+
+import app from '../server/server.js';
+
+export default app; 
